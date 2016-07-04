236e338ef1b60f8c48010ed0961fec49649acea3
Dmitry <dima.shkad01@gamil.com>
2016-07-04 14:15:32 +0300

    Third commit: -added base_types.py

---
+++ base_types.py
@@ -0,0 +1,11 @@
+logic = False
+count = 1000
+test = 888
+miles = 2.3
+name = "John"
+print(logic, not logic)
+print(count, count*miles)
+print(miles, miles/23)
+print(name, "|".join(name))
+print(test,"+",test,"=", test+test)
+
