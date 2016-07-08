c1a4808cc79213b5af965508588ca0b4ad76b13f
Dmitry <dima.shkad01@gamil.com>
2016-07-08 16:48:34 +0300

    Few changes in bmi_calculator.py -simplified program code.

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -4,13 +4,14 @@ height = input("Enter your height(cm)")
 weight = input("Enter your weight(kg)")
 BMI = int(weight)/int(height)/int(height)*10000
 print("Your BMI is ",BMI)
-if(int(BMI)<15):
+if(BMI<15):
 	print("Acute underweight!")
-elif(int(BMI)<20):
+elif(BMI<20):
 	print("Underweight.")
-elif(int(BMI)<25):
+elif(BMI<25):
 	print("Normal weight.")
-elif(int(BMI)<30):
+elif(BMI<30):
 	print("Overweight.")
 else:
 	print("Obesity!")
+
