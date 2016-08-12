7e9f85824374cdf4664c16efd979f5fe0670becf
Dmitry <dima.shkad01@gamil.com>
2016-08-13 00:16:33 +0300

    Added some new files: - analiz_text.py - analiz_text_words.py - file.txt(text)

---
+++ analiz_text_words.py
@@ -0,0 +1,14 @@
+filename = input("Enter a filename: ")
+with open(filename) as f:
+    words = f.read().split()
+f.close()
+d_words = {}
+for word in words:
+    try:
+        d_words[word] += 1
+    except KeyError:
+        d_words[word] = 1
+pairs = sorted(d_words.items(), key = lambda x: x[1], reverse =  True)
+for p in pairs[:10]:
+    print("{} - {} times".format(p[0],p[1]))
+
