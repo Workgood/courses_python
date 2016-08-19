c923f001e868d75fb4e6a9d995edf8b5fae4ca1b
Dmitry <dima.shkad01@gamil.com>
2016-08-20 00:41:03 +0300

    Changed second part in prime_num.py

--- prime_num.py
+++ prime_num.py
@@ -3,15 +3,15 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
-def count_prime(count):
-    prime_numbers = [2]
-    next_number = 3
-    while len(prime_numbers) < count:
-        if is_prime(next_number):
-            prime_numbers.append(next_number)
-        next_number += 1
-    return prime_numbers[-1]
-count = int(input("Enter a number of prime: "))
-print(count_prime(count))
+def count_index(index):
+    cur_number = 2
+    cur_index = 1
+    while cur_index < index:
+        if is_prime(cur_number):
+            cur_index += 1 
+        cur_number += 1
+    return cur_number
+index = int(input("Enter a number of prime: "))
+print(count_index(index))
 
-    
+
