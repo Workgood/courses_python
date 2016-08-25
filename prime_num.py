ddd2e1757f9cb30cd3608f0c7a2204923ebfa9c6
Dmitry <dima.shkad01@gamil.com>
2016-08-25 12:48:44 +0300

    Optimized method of verifying primes in prime_num.py

--- prime_num.py
+++ prime_num.py
@@ -1,5 +1,7 @@
+import math
+
 def is_prime(num):
-    for i in range(2, int(num)):
+    for i in range(2, int(math.sqrt((num))) + 1):
         if num % i == 0:
             return False
     return True
