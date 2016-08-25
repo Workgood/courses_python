2f5e78eac7a85e4f12ef7fd48c4bf72235bb31ec
Dmitry <dima.shkad01@gamil.com>
2016-08-25 12:30:04 +0300

    Fixed bugs in prime_num.py

--- prime_num.py
+++ prime_num.py
@@ -3,14 +3,18 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
+
+
 def count_index(index):
     cur_number = 2
     cur_index = 1
     while cur_index < index:
-        if is_prime(cur_number):
-            cur_index += 1 
         cur_number += 1
+        if is_prime(cur_number):
+            cur_index += 1
     return cur_number
+
+
 index = int(input("Enter a number of prime: "))
 print(count_index(index))
 
