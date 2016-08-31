566f5764ef01e342c5911cb34145d20ef50e60e9
Dmitry <dima.shkad01@gamil.com>
2016-08-31 11:27:02 +0300

    Changed text_wrap.py

--- text_wrap.py
+++ text_wrap.py
@@ -1,3 +1,27 @@
+def wrap(text, maxlen):
+    words = []
+    ntext = ""
+    c = 0
+    for i in text.split():
+        words.append(i)
+    for word in words:
+        c += len(word)
+        if c >= maxlen:
+            ntext += '\n'
+            c = len(word)
+        ntext += " "
+        c += 1
+        ntext += word
+    ntext.split()
+    return ntext
+    
+
+
+with open("text.txt") as f:
+    print(wrap(f.read(),30))
+
+
+'''
 def spl(text, maxlen):
     ntext = ''
     c = 0
@@ -18,4 +42,4 @@ maxlen = int(input("Enter a max length of the line: "))
 with open(filename) as f:
     print(spl(f.read(), maxlen))
 
-
+'''
